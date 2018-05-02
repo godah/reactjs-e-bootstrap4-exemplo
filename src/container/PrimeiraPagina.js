b73bec9a9964b06a507daf3ff575f5f68dfe1548
LucianoLima <yoluciano.fernando@gmail.com>
2018-05-01 22:21:11 -0300

    final do vídeo formulario

--- src/container/PrimeiraPagina.js
+++ src/container/PrimeiraPagina.js
@@ -1,11 +1,77 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import ReleaseForm from '../ui/ReleaseForm';
 
-export default class PrimeiraPagina extends Component{
-    render(){
-        return(
-            <h1>
-                Primeira Pagina
-            </h1>
+export default class PrimeiraPagina extends Component {
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            releases: [
+                {
+                    id: 1,
+                    releaseName: 'Release 01',
+                    releaseDate: '01/01/2018'
+                },
+                {
+                    id: 2,
+                    releaseName: 'Release 02',
+                    releaseDate: '01/01/2019'
+                },
+                {
+                    id: 3,
+                    releaseName: 'Release 03',
+                    releaseDate: '01/01/2020'
+                },
+                {
+                    id: 4,
+                    releaseName: 'Release 04',
+                    releaseDate: '01/01/2021'
+                }
+
+            ]
+        }
+    }
+
+    handleSubmit(e, { name, date }) {
+        e.preventDefault();
+        var state = this.state;
+        var myRelease = {
+            id: state.releases.length + 1,
+            releaseName: name,
+            releaseDate: date
+        }
+
+        this.setState({ releases: state.releases.concat(myRelease) });
+    }
+
+    render() {
+        return (
+            <div className="container" style={{ padding: '25px' }}>
+                <ReleaseForm submitHandler={this.handleSubmit} />
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Relase Name</th>
+                            <th scope="col">Release Date</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.releases.map((release, index) => {
+                            return (
+                                <tr key={release.id}>
+                                    <th scope="row">{release.id}</th>
+                                    <td>{release.releaseName}</td>
+                                    <td>{release.releaseDate}</td>
+                                    <td><button type="button" className="btn btn-outline-danger btn-sm">Remover</button></td>
+                                </tr>
+                            )
+                        })}
+
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
